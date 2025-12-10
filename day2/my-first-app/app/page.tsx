0445a0e5952ee29e745168fbaea17cf369e4f769
Cuong Cao Doan <doancaocuongvn@gmail.com>
2025-12-10 10:07:34 +0700

    Start day 2

---
+++ day2/my-first-app/app/page.tsx
@@ -0,0 +1,17 @@
+import Modal from './ui/modal'
+import Cart from './ui/cart'
+import Carousel from './carousel'
+
+export default function Page() {
+  // return (
+  //   <Modal>
+  //     <Cart />
+  //   </Modal>
+  // )
+  return (
+    <div>
+      <p>View the picture</p>
+      <Carousel />
+    </div>
+  )
+}
