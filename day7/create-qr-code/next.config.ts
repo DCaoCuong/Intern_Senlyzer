44946cd6f6867a4b871043de38b4867ab368fea9
Cuong Cao Doan <doancaocuongvn@gmail.com>
2025-12-15 12:36:41 +0700

    Create QR code app

---
+++ day7/create-qr-code/next.config.ts
@@ -0,0 +1,10 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  //create a consistant for BANK
+  images: {
+    domains: ["qr.sepay.vn"],
+  },
+};
+
+export default nextConfig;
